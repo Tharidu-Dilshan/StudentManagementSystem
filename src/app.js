c5a202ec2d6b4ba99d8e274eed559825b08ac126
Tharidu D Wimalasena <100187445+Tharidu-Dilshan@users.noreply.github.com>
2023-11-20 15:34:58 +0530

    server configeration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("haiyoooo");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv/config");
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>🧑🏻‍🎓 Student Management System API</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is up and running on PORT ${PORT}`);
+});
