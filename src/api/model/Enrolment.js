59c84252e45b6e5227a1f95eb4c897e1ead79a9a
Tharidu D Wimalasena <100187445+Tharidu-Dilshan@users.noreply.github.com>
2023-11-23 20:15:47 +0530

    Model Update

--- src/api/model/Enrolment.js
+++ src/api/model/Enrolment.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Subject = require ('./Subject');
 
 const EnrolmentSchema = new mongoose.Schema({
-    id: String,
-    studentRegistrationNumber: String,
-    subjectCode: String,
+    id: Number,
+    studentRegistrationNumber: { type: Schema.Types.ObjectId, ref: 'Student' },
+    subjectCode: { type: String, ref: Subject },
     status: String
 });
 
--- src/api/model/StudentMark.js
+++ src/api/model/StudentMark.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Enrolment = require('./Enrolment');
+const Grade = require('./Grade');
+
 
 const StudentMarkSchema = new mongoose.Schema({
-    id: String,
-    enrolmentId: String,
-    grade: String
+    id: Number,
+    enrollmentId: { type: Number, ref: Enrolment },
+    grade: { type: String, ref: Grade }
 });
 
 module.exports = mongoose.model('StudentMark', StudentMarkSchema);
