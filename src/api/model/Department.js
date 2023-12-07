9eb9729ada51fc2c236f176262e52d4423eaea79
Tharidu D Wimalasena <100187445+Tharidu-Dilshan@users.noreply.github.com>
2023-12-07 21:48:37 +0530

    Models Modification

--- src/api/model/Department.js
+++ src/api/model/Department.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const DepartmentSchema = new mongoose.Schema({
-    id: String,
-    code: String,
-    name: String
+    
+    code: {type:String ,required:true},
+    name: {type:String ,required:true}
 });
 
 module.exports = mongoose.model('Department', DepartmentSchema);
--- src/api/model/Student.js
+++ src/api/model/Student.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const StudentSchema = new mongoose.Schema({
-    name: String,
-    id: String,
-    age: Number,
-    gender: String,
-    address: String,
-    registerNumber: String
+    name: {type:String ,required:true},
+    age: {type:Number ,required:true},
+    gender: {type:String ,required:true , enum:['male','female']},
+    address: {type:String ,required:true},
+    registerNumber: {
+        type:String,
+        require:true
+    }
 });
 
 module.exports = mongoose.model('Student', StudentSchema);
